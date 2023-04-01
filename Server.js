b0d057b93ded173f30955eb7ec61c0ccab744701
yuvraj <yuvraj042003@gmail.com>
2023-04-01 16:30:50 +0530

    dsfsd

--- Server.js
+++ Server.js
@@ -31,5 +31,5 @@ app.get('/:shortUrl', async (req,res) => {
     
     res.redirect(shortUrl.full)
 })
-app.listen(process.env.PORT || 6000);
+app.listen(process.env.PORT || 5000);
 console.log(`Server is running on `)
