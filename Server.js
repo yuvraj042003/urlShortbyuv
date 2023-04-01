8245ec3c2bef32300627859e2b7cd8d9d95ee396
yuvraj <yuvraj042003@gmail.com>
2023-04-01 21:09:39 +0530

    Shopy

--- Server.js
+++ Server.js
@@ -5,27 +5,27 @@ const app = express()
 const dotenv = require('dotenv');
 
 dotenv.config();
-mongooose.connect(process.env.MONGO, {
-    useNewUrlParser: true, useUnifiedTopology: true })
+mongooose.set("strictQuery", true)
+mongooose.connect(process.env.MONGO)
 
 // hii  sddssnajnjbd
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/', async (req, res)=>{
-    const shortUrls = await ShortUrl.find() 
-    res.render('index', ({shortUrls: shortUrls}));
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', ({ shortUrls: shortUrls }));
 })
-app.post('/shortUrls', async (req,res)=>{
-   await ShortUrl.create({full: req.body.fullUrl})
-   res.redirect('/')
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
+    res.redirect('/')
 })
-app.get('/:shortUrl', async (req,res) => {
-    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl})
-    if(shortUrl == null) return  res.sendStatus(404) 
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
     shortUrl.clicks++
     shortUrl.save()
-    
+
     res.redirect(shortUrl.full)
 })
 app.listen(process.env.PORT || 5000);
